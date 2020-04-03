cda22032c6e6b22beac66ce4d19fcd7530d54aca
lakhoune <38857665+lakhoune@users.noreply.github.com>
2020-04-03 23:21:33 +0200

    added id to LobbySchema

--- models/Lobby.js
+++ models/Lobby.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const LobbySchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: false //false because we will assign it after creation
+  },
   size: {
     type: Number,
     required: true
