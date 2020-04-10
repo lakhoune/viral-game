3f321ee1cb65902af458828bc4fec165cfb10ffa
KuffDeSchmull <kuffer.s7@hotmail.com>
2020-04-11 01:12:23 +0200

    lobbysize, getnames and disconnect implementation, data handling ground work

--- viral-io/src/app/socketio.service.ts
+++ viral-io/src/app/socketio.service.ts
@@ -7,25 +7,29 @@ import {SessionauthService} from './sessionauth.service';
   providedIn: 'root'
 })
 export class SocketioService {
-socket;
-admin;
     
-static lobbyID;
-playerNAME;
+    socket;
+    admin;
     
-constructor(private auth: SessionauthService) {      }
+    static lobbyID;
+    playerNAME;
+    
+    constructor(private auth: SessionauthService) {      }
     
     setupSocketConnection() {
-    this.socket = io(environment.SOCKET_ENDPOINT);
-    this.admin = io(environment.SOCKET_ADMIN_ENDPOINT);
+        this.socket = io(environment.SOCKET_ENDPOINT);
+        this.admin = io(environment.SOCKET_ADMIN_ENDPOINT);
         
         
-            this.admin.on("chat", (data) => {
-    console.log(data);
-  });
-    this.admin.on("log", (data) => {
-    console.log(data);
-  });
+        this.admin.on("chat", (data) => {
+            console.log(data);
+        });
+        this.admin.on("log", (data) => {
+            console.log(data);
+        });
+        this.socket.on("data", (msg) => {  //output data
+          console.log(msg);//store this data as lobby size in a globally accessable variable //satic var of SocketioService
+        });
         this.socket.on("log", (msg) => {    //output server side msgs
             console.log(msg); 
             
@@ -39,45 +43,53 @@ constructor(private auth: SessionauthService) {      }
                 console.log(SessionauthService.readID());
             }
             
-  });
+        });
         this.socket.on("err", (msg) => {  //output error msgs
           console.log(msg);
-  });
+        });
 
   }
     
     
-createLobby(num){
+    createLobby(num){
     
-    let rand = Math.floor(Math.random() * 10000);
-    this.socket.emit("createLobby", rand, num);
-    console.log("test connection here, create adminchat");
+        let rand = Math.floor(Math.random() * 10000);
+        this.socket.emit("createLobby", rand, num);
+        console.log("test connection here, create adminchat");
+        this.getLobbySize();
 
-}
+    }
     
-joinLobby(num){
+    joinLobby(num){
+      
+        if(SessionauthService.readID()){
+            console.log(SessionauthService.readID());
+            console.log(num);
+            this.socket.emit("rejoinLobby", SessionauthService.readID(), num);
     
+        } else {
     
+            this.socket.emit("joinLobby", num);
+            this.getLobbySize();
+        }
     
-if(SessionauthService.readID()){
-    console.log(SessionauthService.readID());
-    console.log(num);
-    this.socket.emit("rejoinLobby", SessionauthService.readID(), num);
-    
-} else {
-    
-    this.socket.emit("joinLobby", num);
-    
-    
-}
-    
-}
+    }
     
     
-setName(name){
+    setName(name){
+        this.socket.emit("setName", name);
+    }   
     
-    this.socket.emit("setName", name);
-}    
+    getLobbySize(){
+        this.socket.emit("getLobbySize");
+        console.log("diarrea");
+    }
+    getNames(id){
+        this.socket.emit("getNames", id);
+    }
+    disconnect(){
+        this.socket.emit("disconnect", "transport close");
+    }
     
 
     
