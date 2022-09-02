3f03fae9ad5163da47e8255e28a90a007d8f5c56
KuffDeSchmull <sven.kuffer@rwth-aachen.de>
2022-09-02 15:21:33 +0200

    revert changes to 625775a and prepare to add dist to gitignore

--- dist/models/player/Role.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class role {
-    constructor(player) {
-        this.player = player;
-    }
-}
-exports.role = role;
-//# sourceMappingURL=Role.js.map
